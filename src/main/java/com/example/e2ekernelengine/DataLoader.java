907f664fd867d52a4a0c9ca9b4d6bc045115bad8
김찬규 <kjkj5868@gmail.com>
2023-11-22 16:12:54 +0900

    chore : 크롤링할 블로그 10개 정도 추가

--- src/main/java/com/example/e2ekernelengine/DataLoader.java
+++ src/main/java/com/example/e2ekernelengine/DataLoader.java
@@ -17,17 +17,9 @@ public class DataLoader {
 	@Autowired
 	public DataLoader(RssCrawler rssCrawler) {
 		this.rssCrawler = rssCrawler;
-		// blogRssUrlList.add("https://hyperconnect.github.io/feed.xml");  // 링크 못 받아옴 -> entry
-		blogRssUrlList.add("https://engineering-skcc.github.io/feed.xml");
-		// blogRssUrlList.add("https://ebay-korea.tistory.com/rss");
-		// blogRssUrlList.add("https://team.postype.com/rss");
-		// blogRssUrlList.add("https://yanolja.github.io/feed");
-		// blogRssUrlList.add("https://meetup.toast.com/rss");
-		// blogRssUrlList.add("https://helloworld.kurly.com/feed.xml");
 
 		blogRssUrlList.add("https://medium.com/feed/@dev29cm");
 		blogRssUrlList.add("https://ridicorp.com/feed/");
-		// blogRssUrlList.add("https://blog.est.ai/feed.xml");
 		blogRssUrlList.add("https://blog.dramancompany.com/feed/");
 		blogRssUrlList.add("https://techblog.yogiyo.co.kr/feed");
 		blogRssUrlList.add("https://blog.hwahae.co.kr/rss.xml");
@@ -45,6 +37,32 @@ public class DataLoader {
 		blogRssUrlList.add("https://blog.banksalad.com/rss.xml");
 		blogRssUrlList.add("https://techblog.woowahan.com/feed/");
 		blogRssUrlList.add("https://tech.inflab.com/rss.xml");
+		blogRssUrlList.add("https://medium.com/feed/coupang-engineering");
+		blogRssUrlList.add("https://tech.devsisters.com/rss.xml");
+		blogRssUrlList.add("https://blog.bespinglobal.com/post/category/aws/feed/");
+		blogRssUrlList.add("https://aws.amazon.com/ko/blogs/tech/feed/");
+		blogRssUrlList.add("https://tech.scatterlab.co.kr/feed");
+		blogRssUrlList.add("https://makinarocks.github.io/feed");
+		blogRssUrlList.add("https://zuminternet.github.io/feed");
+		blogRssUrlList.add("https://medium.com/feed/wantedjobs");
+
+		// blogRssUrlList.add("https://hyperconnect.github.io/feed.xml");  // 링크 못 받아옴 -> entry
+		// blogRssUrlList.add("https://engineering-skcc.github.io/feed.xml"); // 피드 url 주소를 링크가 아니라 id 태그로 줌
+		// blogRssUrlList.add("https://ebay-korea.tistory.com/rss");
+		// blogRssUrlList.add("https://team.postype.com/rss");
+		// blogRssUrlList.add("https://yanolja.github.io/feed");
+		// blogRssUrlList.add("https://meetup.toast.com/rss");
+		// blogRssUrlList.add("https://helloworld.kurly.com/feed.xml");
+		// blogRssUrlList.add("http://feeds.feedburner.com/GoogleDevelopersKorea");
+		// blogRssUrlList.add("https://medium.com/feed/musinsa-tech");
+		// blogRssUrlList.add("https://saramin.github.io/feed");
+		// blogRssUrlList.add("https://tech.kakaoenterprise.com/feed");
+		// blogRssUrlList.add("https://meetup.toast.com/rss");
+		// blogRssUrlList.add("https://dev.gmarket.com/rss");
+		// blogRssUrlList.add("https://tech.kakaoenterprise.com/rss");
+		// blogRssUrlList.add("https://medium.com/feed/class101");
+		// blogRssUrlList.add("https://netmarble.engineering/rss");
+		// blogRssUrlList.add("https://blog.est.ai/feed.xml");
 	}
 
 	public void loadInitialData() throws InterruptedException {
